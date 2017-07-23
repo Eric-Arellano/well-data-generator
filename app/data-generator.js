6014fa9206131a6214823c4ca76cd3a6b6e1793d
Eric Arellano <ericarellano@me.com>
2017-07-23 14:19:22 -0700

    implement data-generator

---
+++ app/data-generator.js
@@ -0,0 +1,67 @@
+// ---------------------------------
+// Generate individual attributes
+// ---------------------------------
+
+class Community {
+  constructor(name, latitude, longitude) {
+    this.name = name;
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+}
+
+const communities = [
+  new Community('Wusuta Sakakyere', 6.822487, 0.246398),
+  new Community('Kpando', 6.862349, -0.282640),
+  new Community('Adawso', 6.517805, -0.270774),
+  new Community('Gonja', 8.215568, -0.436964),
+  new Community('Katua', 9.945856, -1.953077),
+];
+
+function getCommunity(name) {
+  return communities.find(community => community.name === name);
+}
+
+function getRandomCommunityName() {
+  return communities[Math.floor(Math.random() * communities.length)].name;
+}
+
+function generateMessageId() {
+  return `_${Math.random().toString(36).substr(2, 9)}`;
+}
+
+function generateDatapoints(numDatapoints) {
+  const datapoints = [];
+  for (let i = 0; i < numDatapoints; i += 1) {
+    const timestamp = 101 * i; // TODO: implement timestamp
+    const value = Math.random(); // TODO: implement random value
+    datapoints.push([timestamp, value]);
+  }
+  return datapoints;
+}
+
+// ---------------------------------
+// Generate JSON record
+// ---------------------------------
+
+module.exports = {
+  generateJsonForGivenCommunity(name) {
+    const community = getCommunity(name);
+    const message = {
+      messageId: generateMessageId(),
+      body: [{
+        name: 'well_hourly_water_level',
+        datapoints: generateDatapoints(4),
+        attributes: {
+          community: community.name,
+          latitude: community.latitude,
+          longitude: community.longitude,
+        },
+      }],
+    };
+    return JSON.stringify(message);
+  },
+  generateJsonForRandomCommunity() {
+    return module.exports.generateJsonForGivenCommunity(getRandomCommunityName());
+  },
+};
--- app/server.js
+++ app/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const dataGenerator = require('./data-generator');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/api/random', (req, res) => {
+  res.send(dataGenerator.generateJsonForRandomCommunity());
 });
 
 app.listen(3000, () => {
