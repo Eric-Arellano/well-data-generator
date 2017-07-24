b043ddf7b001024f6cf9157ccf70303fe992dc13
Eric Arellano <ericarellano@me.com>
2017-07-23 22:20:17 -0700

    use normal distribution & sigmoid function to generate better values

--- app/data-generator.js
+++ app/data-generator.js
@@ -1,7 +1,7 @@
 const faker = require('faker');
 
 // ---------------------------------
-// Generate individual attributes
+// Generate attributes
 // ---------------------------------
 
 class Community {
@@ -32,30 +32,74 @@ function generateMessageId() {
   return faker.random.uuid();
 }
 
+
+// ---------------------------------
+// Generate data
+// ---------------------------------
+
 function generateTimestamp(hourOfDay) {
   const millisecondTimestamp = Date.now();
   const unixTimestamp = Math.floor(millisecondTimestamp / 1000);
   return unixTimestamp + (hourOfDay * 3600);
 }
 
+Math.normalizedRandom = () => {
+  let x1;
+  let x2;
+  let rad;
+  do {
+    x1 = (2 * Math.random()) - 1;
+    x2 = (2 * Math.random()) - 1;
+    rad = (x1 * x1) + (x2 * x2);
+  } while (rad >= 1 || rad === 0);
+  const c = Math.sqrt((-2 * Math.log(rad)) / rad);
+  return x1 * c;
+};
+
 // value from 35 - 45, with two decimals
 function generateFunctioningValue() {
-  const randomValue = (Math.random() * 10) + 35.0;
-  return Math.round(randomValue * 1e2) / 1e2;
+  // constants
+  const BASE_VALUE = 35.0;
+  const VARIANCE_AMOUNT = 7;
+  const NUMBER_DECIMAL_PLACES = 1e2;
+  // normally distributed value
+  const randomValue = (Math.normalizedRandom() * VARIANCE_AMOUNT) + BASE_VALUE;
+  // round and absolute value
+  const roundedValue = Math.round(randomValue * NUMBER_DECIMAL_PLACES) / NUMBER_DECIMAL_PLACES;
+  return Math.abs(roundedValue);
 }
 
-// value from 0 - 2, with two decimals
-function generateBrokenValue() {
-  const randomValue = (Math.random() * 2);
-  return Math.round(randomValue * 1e2) / 1e2;
+// sigmoid function, starting at 35 then decaying to 0-2
+function generateBrokenValue(index) {
+  // constants
+  const STARTING_BASE_VALUE = 35;
+  const BEGIN_DECAY_INDEX = 10;
+  const DECAY_RATE = 2.5;
+  const NUMBER_DECIMAL_PLACES = 1e2;
+  // sigmoid formula
+  const sigmoidDecayValue = STARTING_BASE_VALUE /
+    (1 + (Math.E ** ((DECAY_RATE * index) - BEGIN_DECAY_INDEX)));
+  // add noise
+  let sigmoidDecayValueWithNoise;
+  if (sigmoidDecayValue > 20) {
+    const VARIANCE_AMOUNT = 5;
+    sigmoidDecayValueWithNoise = sigmoidDecayValue + (Math.normalizedRandom() * VARIANCE_AMOUNT);
+  } else {
+    const VARIANCE_AMOUNT = 2;
+    sigmoidDecayValueWithNoise = sigmoidDecayValue + (Math.random() * VARIANCE_AMOUNT);
+  }
+  // round and absolute value
+  const roundedValue = Math.round(sigmoidDecayValueWithNoise * NUMBER_DECIMAL_PLACES)
+    / NUMBER_DECIMAL_PLACES;
+  return Math.abs(roundedValue);
 }
 
-// TODO: Is there a better way to generate both broken & functioning data?
-function generateDatapoints(numDatapoints, isNotBroken) {
+function generateDatapoints(numDatapoints, isBroken = false) {
   const datapoints = [];
+  console.log(isBroken);
   for (let i = 0; i < numDatapoints; i += 1) {
     const timestamp = generateTimestamp(i);
-    const value = isNotBroken ? generateFunctioningValue() : generateBrokenValue();
+    const value = isBroken ? generateBrokenValue(i) : generateFunctioningValue();
     datapoints.push([timestamp, value]);
   }
   return datapoints;
@@ -84,10 +128,10 @@ function generateJson(communityName, datapoints) {
 
 module.exports = {
   generateFunctioningData(name) {
-    return generateJson(name, generateDatapoints(4, true));
+    return generateJson(name, generateDatapoints(24));
   },
   generateBrokenData(name) {
-    return generateJson(name, generateDatapoints(4, false));
+    return generateJson(name, generateDatapoints(24, true)); // true means well is broken
   },
   getRandomCommunityName() {
     return getRandomCommunityName();
